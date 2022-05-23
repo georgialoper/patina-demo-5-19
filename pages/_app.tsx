3d8dcc1adbf68b60513de6df59414705a989b9b0
Georgia Loper <georgia.loper@upstart.com>
2022-05-23 14:47:23 -0400

    Patina demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import '@upstart/patina-design-system/css-reset'
 import type { AppProps } from 'next/app'
+import { PatinaProvider } from '@upstart/patina-design-system'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PatinaProvider>
+      <Component {...pageProps} />
+    </PatinaProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/index.css.ts
@@ -0,0 +1,5 @@
+import { style } from "@vanilla-extract/css";
+
+export const bgImage = style({
+  backgroundImage: "url('https://staging1.upstart.com/webpack/production/home/homepage_hero_tiles_md-054cf3e13058ad3ac11d7a49acb11e65.jpeg')"
+})
